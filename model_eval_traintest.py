13dc18e4293b5cd6f4b75d834bcdfc3a8a896a3c
makunmadar <makun.madar@kromek.com>
2023-07-04 14:55:30 +0100

    Modifying analysis to ignore zero truth values.

--- model_eval_traintest.py
+++ model_eval_traintest.py
@@ -3,6 +3,7 @@ from numpy import genfromtxt
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from joblib import load
+from sklearn.metrics import mean_absolute_error
 
 X_test = np.load('Data/Data_for_ML/testing_data/X_test.npy')
 y_test = np.load('Data/Data_for_ML/testing_data/y_test.npy')
@@ -18,39 +19,34 @@ yhat = scaler_label.inverse_transform(yhat)
 
 bin_file = 'Data/Data_for_ML/bin_data/bin_sub12_dndz'
 bins = genfromtxt(bin_file)
+bins_k = bins[13:22]
 
 # Manual redshift distribution MAE score
-nz = len(bins[0:13])
-Nz = len([i[0:13] for i in y_test])
 y_testz = [i[0:13] for i in y_test]
 yhatz = [i[0:13] for i in yhat]
 
 MAEz = []
-for j in range(Nz):
-    maei = 0
-    for i in range(nz):
-        sumi = abs(y_testz[j][i] - yhatz[j][i])
-        maei += sumi
-    maei = maei / nz
-    if maei > 0.3:
+for j in range(200):
+    maei = mean_absolute_error(y_testz[j], yhatz[j])
+    if maei > 0.2:
         print("Model ", j + 1, "had dn/dz MAE: ", maei)
     MAEz.append(maei)
 
 # Manual luminosity function MAE score
-
-nk = len(bins[13:22])
-Nk = len([i[13:22] for i in y_test])
 y_testk = [i[13:22] for i in y_test]
 yhatk = [i[13:22] for i in yhat]
 
+yhatk = [row_a[row_b != 0] for row_a, row_b in zip(yhatk, y_testk)]
+binsk = []
+for i in range(200):
+    bk = bins_k[y_testk[i] != 0]
+    binsk.append(bk)
+y_testk = [row[row != 0] for row in y_testk]
+
 MAEk = []
-for j in range(Nk):
-    maei = 0
-    for i in range(nk):
-        sumi = abs(y_testk[j][i] - yhatk[j][i])
-        maei += sumi
-    maei = maei / nk
-    if maei > 0.3:
+for j in range(200):
+    maei = mean_absolute_error(y_testk[j], yhatk[j])
+    if maei > 0.7:
         print("Model ", j + 1, "had LF_K MAE: ", maei)
     MAEk.append(maei)
 
@@ -71,7 +67,7 @@ fig, axs = plt.subplots(2, 3, figsize=(15, 10),
 fig.subplots_adjust(wspace=0)
 axs = axs.ravel()
 
-m = 66
+m = 138
 for i in range(6):
     axs[i].plot(bins[0:13], yhatz[i+m], '--', label=f"Prediction MAE: {MAEz[i+m]:.3f}", alpha=0.5)
     axs[i].plot(bins[0:13], y_testz[i+m], 'gx-', label="True model "+str(i+1+m))
@@ -86,7 +82,7 @@ fig, axs = plt.subplots(2, 3, figsize=(15, 10),
 fig.subplots_adjust(wspace=0)
 axs = axs.ravel()
 
-m = 182
+m = 180
 for i in range(6):
     axs[i].plot(bins[0:13], yhatz[i+m], '--', label=f"Prediction MAE: {MAEz[i+m]:.3f}", alpha=0.5)
     axs[i].plot(bins[0:13], y_testz[i+m], 'gx-', label="True model "+str(i+1+m))
@@ -101,10 +97,10 @@ fig, axs = plt.subplots(2, 3, figsize=(15, 10),
 fig.subplots_adjust(wspace=0)
 axs = axs.ravel()
 
-m = 66
+m = 152
 for i in range(6):
-    axs[i].plot(bins[13:22], yhatk[i+m], '--', label=f"Prediction MAE: {MAEk[i+m]:.3f}", alpha=0.5)
-    axs[i].plot(bins[13:22], y_testk[i+m], 'gx-', label="True model "+str(i+1+m))
+    axs[i].plot(binsk[i+m], yhatk[i+m], '--', label=f"Prediction MAE: {MAEk[i+m]:.3f}", alpha=0.5)
+    axs[i].plot(binsk[i+m], y_testk[i+m], 'gx-', label="True model "+str(i+1+m))
     axs[i].legend()
     axs[i].set_xlabel("Redshift, z", fontsize=16)
 
@@ -116,10 +112,10 @@ fig, axs = plt.subplots(2, 3, figsize=(15, 10),
 fig.subplots_adjust(wspace=0)
 axs = axs.ravel()
 
-m = 182
+m = 165
 for i in range(6):
-    axs[i].plot(bins[13:22], yhatk[i+m], '--', label=f"Prediction MAE: {MAEk[i+m]:.3f}", alpha=0.5)
-    axs[i].plot(bins[13:22], y_testk[i+m], 'gx-', label="True model "+str(i+1+m))
+    axs[i].plot(binsk[i+m], yhatk[i+m], '--', label=f"Prediction MAE: {MAEk[i+m]:.3f}", alpha=0.5)
+    axs[i].plot(binsk[i+m], y_testk[i+m], 'gx-', label="True model "+str(i+1+m))
     axs[i].legend()
     axs[i].set_xlabel("Redshift, z", fontsize=16)
 
